fbefc0760d8181fbdb139b77fd849994574d8538
Andrew Jones <joneseyit@protonmail.com>
2018-11-22 20:31:23 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 
 require_relative '../../config/environment'
-
+require 'pry'
 class ApplicationController < Sinatra::Base
+
+  #What order do these things go in
+
+
 
   configure do
     set :public_folder, 'public'
@@ -9,6 +13,53 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    erb :index
+  end
 
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    @post = Post.create(params)
+    redirect to "/posts"
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+
+  get '/show' do
+    erb:index
+  end
+
+  get '/posts' do
+    @posts = Post.all
+  end
+
+  get '/posts/:id' do
+    @post = Post.all.find(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.all.find(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    @post = Post.all.find(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :show
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.all.find(params[:id])
+    @post.delete
+    erb :delete
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,44 @@
 #Placeholder for a model
-
 class Post < ActiveRecord::Base
+  
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# class Post < ActiveRecord::Base
+# end
